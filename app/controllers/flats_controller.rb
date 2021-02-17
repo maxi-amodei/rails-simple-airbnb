ceec65f80e8f1980bf668b8de5a49bb5777a7a73
Maxi Amodei <maxi_amodei@hotmail.com>
2021-02-17 15:37:53 -0300

    Created 7 routes and controlles methods defined, not created

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,44 @@
+class FlatsController < ApplicationController
+  def index
+    
+  end
+
+  def show
+    
+  end
+
+  def new
+   
+    # needed to instantiate the form_for
+  end
+
+  def create
+    # @restaurant = Restaurant.new(restaurant_params)
+    # if @restaurant.save
+    #   redirect_to restaurant_path(@restaurant)
+    # else
+    #   render :new
+    # end
+  end
+
+  def edit
+   
+    
+  end
+
+  def update
+   
+    
+  end
+
+  def destroy
+   
+    
+  end
+
+  private
+
+  # def flats_params
+  #   params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  # end
+end
